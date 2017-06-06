b1a9197934a8dbe419d0d290a484ee55b51f55af
Sam Dowling <sam.dowling@students.ittralee.ie>
2017-06-06 23:09:17 +0100

    Restructured AngularJS code.

--- Client/Web/js/inbox.js
+++ Client/Web/js/inbox.js
@@ -1,21 +1,47 @@
-var securemail = angular.module('securemail', []);
+angular.module('securemail', ['ngRoute', 'ngResource'])
 
-securemail.controller('Register', function($scope, $http) {
-  $scope.registerClick = function(){
-    $http.get('http://86.45.70.108:8080/register').
-        then(function(response) {
-            $scope.register = response.data;
-            window.alert("Details: "+$scope.register);
-        });
-  }
-});
+  .factory('SecureMail', function($resource) {
+    var SecureMail = $resource(':protocol//:server/:a/:b', {
+      protocol: 'http:',
+      server: '86.45.70.108:8080'
+    });
 
-securemail.controller('RequestInbox', function($scope, $http) {
-  $scope.requestInboxClick = function(){
-    $http.get('http://86.45.70.108:8080/{'+$scope.id+'}:{'+$scope.secret+'}').
-        then(function(response) {
-            $scope.response = response.data;
-            window.alert("Inbox: "+$scope.response);
-        });
-  }
-});
+    return SecureMail;
+  })
+
+
+  .config(function($routeProvider) {
+    $routeProvider
+    .when('/', {
+      controller: 'RegisterCtrl',
+      templateUrl: 'signin.html'
+    })
+      .when('/register', {
+        controller: 'RegisterCtrl',
+        templateUrl: 'signin.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('');
+  }])
+
+  .controller('RegisterCtrl', function($scope, SecureMail) {
+
+    // $scope.requestInboxClick = function(){
+    //   $http.get('http://86.45.70.108:8080/{'+$scope.id+'}:{'+$scope.secret+'}').
+    //       then(function(response) {
+    //           $scope.response = response.data;
+    //           window.alert("Inbox: "+$scope.response);
+    //       });
+    // },
+
+    $scope.Register = function(){
+      $scope.RegisterDetails = SecureMail.get({
+        a: 'register'
+      });
+    };
+  });
